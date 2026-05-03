9475b5b2457d75c94676ccd134ad137ec2df266f
Guilherme <guiboromello@gmail.com>
2026-05-03 19:51:37 -0300

    feat: create header component

---
+++ src/components/layout/Header/Header.tsx
@@ -0,0 +1,15 @@
+import { Landmark } from "lucide-react"
+
+
+export default function Header() {
+    return (
+      <header className="flex justify-between items-center">
+        <div className="flex gap-2 items-center">
+          <Landmark size={24} color="#000" />
+          <h1 className="text-lg">Fortis</h1>
+        </div>
+
+        <p className="text-sm">01/01/2026</p>
+      </header>
+    );
+}
