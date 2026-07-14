ac7ec29f3595f5e7de6756af19830d32db9d70af
Guilherme <guiboromello@gmail.com>
2026-07-14 12:13:18 -0300

    fix: file type

--- src/components/ui/TransactionModal/TransactionModal.tsx
+++ src/components/ui/TransactionModal/TransactionModal.tsx
@@ -193,11 +193,6 @@ export default function TransactionModal({
                             className="h-8 w-full rounded-lg border border-input bg-transparent px-2.5 py-1 text-sm outline-none file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:cursor-not-allowed disabled:opacity-50"
                             {...register("file")}
                         />
-                        {errors.file && (
-                            <p className="text-xs text-danger">
-                                {errors.file.message}
-                            </p>
-                        )}
                     </div>
                 </div>
 
